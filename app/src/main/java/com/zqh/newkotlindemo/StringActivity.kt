3e7d541715a4e5e9026998289e226479426691d0
zqh <zqh414500412@gmail.com>
2019-03-12 16:32:29 +0800

    add string substring/replace/split

--- app/src/main/java/com/zqh/newkotlindemo/StringActivity.kt
+++ app/src/main/java/com/zqh/newkotlindemo/StringActivity.kt
@@ -8,7 +8,7 @@ import android.widget.Button
 import android.widget.TextView
 
 
-const val str = "girl i like you"
+const val str = "1girl i 2like you"
 
 /**
  * 字符串操作相关
@@ -19,13 +19,15 @@ class StringActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var mShowTextview: TextView
     private lateinit var mFindStringBtn: Button
     private lateinit var mSubStringBtn: Button
+    private lateinit var mResplaceBtn: Button
+    private lateinit var mSplitBtn: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_string)
 
-        title="String edit"
+        title = "String edit"
         initView()
     }
 
@@ -33,10 +35,14 @@ class StringActivity : AppCompatActivity(), View.OnClickListener {
         mShowTextview = findViewById(R.id.string_show_tv)
         mFindStringBtn = findViewById(R.id.string_findstring_btn)
         mSubStringBtn = findViewById(R.id.string_substring_btn)
+        mResplaceBtn = findViewById(R.id.string_replacestring_btn)
+        mSplitBtn = findViewById(R.id.string_splitstring_btn)
 
         mShowTextview.setOnClickListener(this)
         mFindStringBtn.setOnClickListener(this)
         mSubStringBtn.setOnClickListener(this)
+        mResplaceBtn.setOnClickListener(this)
+        mSplitBtn.setOnClickListener(this)
 
 
     }
@@ -52,6 +58,12 @@ class StringActivity : AppCompatActivity(), View.OnClickListener {
             }
             R.id.string_substring_btn -> {
                 subString()
+            }
+            R.id.string_replacestring_btn -> {
+                replaceString()
+            }
+            R.id.string_splitstring_btn -> {
+                splitString()
             }
         }
     }
@@ -82,15 +94,40 @@ class StringActivity : AppCompatActivity(), View.OnClickListener {
     /**
      * 截取str文本中的字段
      */
-    fun subString(){
-        var subStr=str.substring(0,5)//0到5，不包括5
-        var subRang= str.substring(IntRange(0,5))//range 包括5这个位置
-        var showText="subString str: $str \n sub(0-5): $subStr \n sub rang(0-5): $subRang"
-        mShowTextview.text=showText
+    fun subString() {
+        var subStr = str.substring(0, 5)//0到5，不包括5
+        var subRang = str.substring(IntRange(0, 5))//range 包括5这个位置
+        var showText = "subString str: $str \n sub(0-5): $subStr \n sub rang(0-5): $subRang"
+        mShowTextview.text = showText
     }
 
-    fun replaceString(){
+    /**
+     * 替换字符串
+     */
+    fun replaceString() {
+        var relacestr = str.replace("like", "love")
+        var showText = "replace $str like to love:\n $relacestr"
+        mShowTextview.text = showText
 
     }
 
+    /**
+     * 分割str。
+     * <p> 分割根据空格
+     * <p> 分割根据数字的正则
+     */
+    fun splitString() {
+        var splitSpaceStr = str.split(" ")
+        var showSplitSpaceStr = "split '$str' by space: \n "
+        for (i in splitSpaceStr) {
+            showSplitSpaceStr = showSplitSpaceStr.plus(i)
+        }
+        showSplitSpaceStr = showSplitSpaceStr.plus("\n split '$str' by [0-9]: \n ")
+        var splitRegexStr = str.split(Regex("[0-9]+"))
+        for (i in splitRegexStr) {
+            showSplitSpaceStr = showSplitSpaceStr.plus(i)
+        }
+
+        mShowTextview.text = showSplitSpaceStr
+    }
 }
